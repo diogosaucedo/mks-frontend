db7c0dab4f8e2c6cb152e202d846dd5e224dc9e1
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-02-01 12:12:50 -0400

    feat: add application business logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
+'use client';
+
+import { NavBar } from '@/components';
 import { Montserrat } from '@next/font/google';
 
 import HomeLayoutProps from './types';
+import { GlobalStyles } from '@/themes';
+import { Cart } from '@/features';
+import { Footer } from '@/components/Navigation/Footer/Footer';
+
+import { store } from '@/store/';
+import { Provider } from 'react-redux';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -8,7 +17,15 @@ const HomeLayout = (props: HomeLayoutProps) => {
   return (
     <html lang="pt-br">
       <head />
-      <body className={montserrat.className}>{props.children}</body>
+      <body className={montserrat.className}>
+        <GlobalStyles />
+        <Provider store={store}>
+          <NavBar />
+          <Cart />
+          {props.children}
+          <Footer></Footer>
+        </Provider>
+      </body>
     </html>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,36 @@
 'use client';
+import { Center, Flex } from '@/components';
+import { Product } from '@/features';
+import { IProduct } from '@/interfaces';
+import { use } from 'react';
 
-import { GlobalStyles } from '@/themes';
+const getProducts = async (): Promise<IProduct[] | undefined> => {
+  const url =
+    'https://mks-challenge-api-frontend.herokuapp.com/api/v1/products/?page=1&rows=8&sortBy=id&orderBy=DESC';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data?.products;
+};
 
 const Home = () => {
+  const products = use(getProducts());
+
+  const flexProps = {
+    width: '100%',
+    maxWidth: '938px',
+    flexWrap: 'wrap',
+    gap: '20px',
+  };
+
   return (
     <>
-      <GlobalStyles />
-      <p>Home</p>
+      <Center width="100%" height="100%">
+        <Flex {...flexProps}>
+          {products?.map((product) => (
+            <Product key={product.id} data={product} />
+          ))}
+        </Flex>
+      </Center>
     </>
   );
 };
