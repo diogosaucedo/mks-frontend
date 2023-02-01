32f532fa6ad081c7f8b14030aa6802f612fa894a
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-02-01 12:07:38 -0400

    feat: add product feature

---
+++ src/features/Product/Product.tsx
@@ -0,0 +1,93 @@
+import {
+  Button,
+  Card,
+  Flex,
+  Heading,
+  Highlight,
+  Image,
+  Stack,
+  Text,
+} from '@/components';
+import React from 'react';
+import { ProductProps } from './type';
+import { colors } from '@/themes';
+
+import { useDispatch } from 'react-redux';
+import { addProduct, counter } from '@/store/Stock.store';
+
+export const Product = (props: ProductProps) => {
+  const dispatch = useDispatch();
+  return (
+    <Card
+      key={props.data.id}
+      width="218px"
+      height="285px"
+      borderRadius="8px"
+      flexDirection="column"
+      justifyContent="space-between"
+      boxShadow="0px 2px 8px rgba(0, 0, 0, 0.135216);"
+    >
+      <Stack
+        flexDirection="column"
+        padding="14px"
+        gap="6px"
+        alignItems="center"
+      >
+        <Image
+          src={props.data.photo}
+          alt={props.data.name}
+          height="138px"
+          width="fit-content"
+        />
+        <Flex width="100%" justifyContent="space-between" alignItems="start">
+          <Heading
+            width="120px"
+            height="38px"
+            type="h2"
+            fontSize="16px"
+            fontWeight={400}
+            color={colors.darkGray}
+          >
+            {props.data.name}
+          </Heading>
+          <Highlight
+            padding="4px 6px"
+            background={colors.darkGray}
+            color={colors.white}
+            fontSize="15px"
+            borderRadius="5px"
+            fontWeight={700}
+          >
+            R${Math.trunc(props.data.price)}
+          </Highlight>
+        </Flex>
+        <Text width="190px" height="25px" fontSize="10px" fontWeight={300}>
+          {props.data.description}
+        </Text>
+      </Stack>
+      <Button
+        background={colors.darkBlue}
+        border="none"
+        width="100%"
+        height="32px"
+        borderRadius="0 0 8px 8px"
+        onClick={() => {
+          dispatch(addProduct({ ...props.data, amount: 1 }));
+          dispatch(counter());
+        }}
+      >
+        <Stack gap="14px" alignItems="center">
+          <Image
+            src={'shopping-bag.svg'}
+            alt="Shopping bag icon"
+            width="13px"
+            height="fit-content"
+          />
+          <Text color={colors.white} fontWeight={600} fontSize="14px">
+            COMPRAR
+          </Text>
+        </Stack>
+      </Button>
+    </Card>
+  );
+};
---
+++ src/features/Product/type.ts
@@ -0,0 +1,8 @@
+import { IProduct } from '@/interfaces';
+
+type ProductProps = {
+  children?: React.ReactNode;
+  data: IProduct;
+  key?: number | string;
+};
+export type { ProductProps };
